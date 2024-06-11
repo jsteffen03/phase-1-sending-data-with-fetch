1d77816c32bdb9cc633785df8da39c64647f9d9b
Justin Steffen <jcsteffen03@gmail.com>
2024-06-10 20:55:12 -0600

    Test Complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+    function submitData(name, email){
+        return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({ name: name, email: email })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const newId = data.id;
+            const idElement = document.createElement('p');
+            idElement.textContent = `New ID: ${newId}`;
+            document.body.appendChild(idElement);  
+        })
+        .catch((error)=> {
+            alert("I Need Help!")
+            const errorElement = document.createElement('p');
+            errorElement.textContent = error.message;
+            document.body.appendChild(errorElement);
+        })
+       
+    }    
+    
+submitData()
+
